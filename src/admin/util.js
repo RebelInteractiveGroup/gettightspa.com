a4556317d5ea6f876f30ff12320d055a4391bf0e
Kyle Sheldon <tbcsheldon@gmail.com>
2020-11-12 16:26:37 -0500

    :bug: module export bug fixes

--- src/admin/util.js
+++ src/admin/util.js
@@ -1,10 +1,10 @@
+import jquery from 'jquery';
+import popper from 'popper.js';
+import bootstrap from 'bootstrap';
 import helpers from '../_data/helpers';
 import dateFilter from '../filters/date-filter';
 import markdownFilter from '../filters/markdown-filter';
 import w3DateFilter from '../filters/w3-date-filter';
-import jquery from 'jquery';
-import popper from 'popper.js';
-import bootstrap from 'bootstrap';
 
 export {
   jquery,
